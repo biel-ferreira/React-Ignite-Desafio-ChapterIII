9d6fe727c49adf3b7d72f1c4463fe9a444c6048f
Gabriel Araujo Ferreira da Silva <gabriel.ferreira@nextpage.com.br>
2022-05-29 19:58:23 -0300

    First Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  console.log("oi");
+  
+
   return <Component {...pageProps} />;
 }
 
